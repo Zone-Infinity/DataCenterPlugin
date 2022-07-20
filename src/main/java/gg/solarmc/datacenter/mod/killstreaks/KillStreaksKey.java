2c5f4828c58044cf163436bc04e9afda5a4003f5
Zone-Infinity <sohamdokhale1602@gmail.com>
2022-07-20 22:11:05 +0530

    add getTopKillStreaks method to KillStreaks

--- src/main/java/gg/solarmc/datacenter/mod/killstreaks/KillStreaksKey.java
+++ src/main/java/gg/solarmc/datacenter/mod/killstreaks/KillStreaksKey.java
@@ -5,8 +5,13 @@ import gg.solarmc.datacenter.database.data.Column;
 import gg.solarmc.datacenter.database.data.single.SingleDataConstants;
 import gg.solarmc.datacenter.database.data.single.SingleDataKey;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class KillStreaksKey implements SingleDataKey<KillStreaks, Integer> {
     public static final KillStreaksKey INSTANCE = new KillStreaksKey();
@@ -15,12 +20,12 @@ public class KillStreaksKey implements SingleDataKey<KillStreaks, Integer> {
     private final SingleDataConstants constants;
 
     private KillStreaksKey() {
-        constants = new SingleDataConstants("killstreaks", new Column("streak", "INTEGER", "0"));
+        constants = new SingleDataConstants("kill_streaks", new Column("top_streak", "INTEGER", "0"));
     }
 
     @Override
     public String getName() {
-        return "killstreaks";
+        return "kill_streaks";
     }
 
     @Override
@@ -31,6 +36,33 @@ public class KillStreaksKey implements SingleDataKey<KillStreaks, Integer> {
     @Override
     public KillStreaks getData(DataCenter center, String uuid) {
         return new KillStreaks(center, uuid, cache.get(uuid));
+    }
+
+    public Map<UUID, Integer> getTopKillStreaks(DataCenter center, int limit) {
+        try (Connection connection = center.getConnection();
+             PreparedStatement statement = connection.prepareStatement(String.format(
+                     "SELECT %s, %s FROM %s ORDER BY %s DESC LIMIT %s",
+                     constants.getUUIDName(),
+                     constants.getValueName(),
+                     constants.getTableName(),
+                     constants.getValueName(),
+                     limit
+             ))
+        ) {
+            Map<UUID, Integer> killStreaks = new HashMap<>();
+            try (ResultSet result = statement.executeQuery()) {
+                while (result.next()) {
+                    UUID uuid = UUID.fromString(result.getString(constants.getUUIDName()));
+                    int streak = result.getInt(constants.getValueName());
+                    killStreaks.put(uuid, streak);
+                }
+            }
+
+            return killStreaks;
+        } catch (SQLException e) {
+            e.printStackTrace(); // for better debugging
+            throw new RuntimeException("KillStreaksKey#getTopKillStreaks caught an exception");
+        }
     }
 
     @Override
